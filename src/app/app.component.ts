38d517feeb769120cd5517fcb26553056fa1b066
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-12-07 20:51:36 +0200

    add mock components, types, services & app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CellComponent } from './components/cell/cell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CellComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
