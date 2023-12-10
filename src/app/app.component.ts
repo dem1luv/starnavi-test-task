48a41e665c43beb5f9c4d5e12c8d12cace33bfe0
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-12-10 13:37:58 +0200

    add pick mode select with dynamics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,36 @@ import { CellComponent } from './components/cell/cell.component';
 import { Mode } from './types/mode.interface';
 import { ModeService } from './services/mode.service';
 import { take } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CellComponent],
+  imports: [CommonModule, CellComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  modes: Mode[] = [];
+  public modes: Mode[] = [];
+  public size: number = 5;
+
+  public selectedModeId: string = '';
 
   constructor(
     private readonly modeService: ModeService
   ) {
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
     this.modeService.getModes().pipe(take(1)).subscribe(modes => this.modes = modes);
+  }
+
+  public onStart(): void {
+    const mode = this.modes.find(mode => mode.id === this.selectedModeId);
+    if (!mode) {
+      return;
+    }
+
+    this.size = mode.field;
   }
 }
--- src/app/services/mode.service.ts
+++ src/app/services/mode.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Mode } from '../types/mode.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ModeService {
   ) {
   }
 
-  getModes() {
+  public getModes(): Observable<Mode[]> {
     return this.http.get<Mode[]>('https://60816d9073292b0017cdd833.mockapi.io/modes');
   }
 }
